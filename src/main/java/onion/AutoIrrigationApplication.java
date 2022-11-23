ceb0005d35f9ac156658274e50ee31dfeb1d908a
Xavier Mino <xavier.mino@justeattakeaway.com>
2022-11-22 21:22:53 -0600

    setup projet and archiitecture

---
+++ src/main/java/onion/AutoIrrigationApplication.java
@@ -0,0 +1,14 @@
+package onion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AutoIrrigationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoIrrigationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autoirrigation/AutoIrrigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoirrigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoIrrigationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
